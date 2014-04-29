75aa30ff267ef5960c28019d817e583e1fe316cb
Kevin Williams <kevin@isolationism.com>
2014-04-29 16:26:16 -0400

    Version bump for dependency update

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     name='django-cleaver',
     author='Kevin Williams',
     author_email='kevin@weblivion.com',
-    version='0.2.5',
+    version='0.2.6',
     license='BSD',
     url='https://github.com/isolationism/django-cleaver',
     download_url='https://github.com/isolationism/django-cleaver/tarball/master',
     description='Integrates CleverCSS with Django with built-in support for franchise customisations',
     packages=find_packages(),
     include_package_data = False,
-    install_requires = ['clevercss>=0.2.2.dev'],
+    install_requires = ['clevercss>=0.2.3.dev'],
 )
 
